7278ee67b537a67e031b6de255158b2d7a4420e3
thakuratul2 <pratapsinghatul111@gmail.com>
2023-11-26 12:21:01 +0530

    Product Image Added

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -257,5 +257,26 @@
                 }
             });
         });
+        Dropzone.autoDiscover = false;    
+const dropzone = $("#image").dropzone({ 
+    init: function() {
+        this.on('addedfile', function(file) {
+            if (this.files.length > 1) {
+                this.removeFile(this.files[0]);
+            }
+        });
+    },
+    url:  "{{ route('temp-images.create') }}",
+    maxFiles: 10,
+    paramName: 'image',
+    addRemoveLinks: true,
+    acceptedFiles: "image/jpeg,image/png,image/gif",
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }, success: function(file, response){
+        $("#image_id").val(response.image_id);
+        //console.log(response)
+    }
+});
     </script>
 @endsection
